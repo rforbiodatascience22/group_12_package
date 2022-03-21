2ef3d4ca78d6bf3affad71303739adb7ca777149
jonfunk21 <funk.jonathan21@gmail.com>
2022-03-21 09:34:38 +0000

    added random sequence function

---
+++ R/function1.R
@@ -0,0 +1,13 @@
+#' Create a random sequence of length n
+#'
+#' @param n 
+#'
+#' @return random_sequence
+#' @export
+#'
+#' @examples random_sequence(10)
+random_seq <- function(n){
+  nucleotides <- sample(c("A", "T", "G", "C"), size = n, replace = TRUE)
+  random_sequence <- paste0(nucleotides, collapse = "")
+  return(random_sequence)
+}
