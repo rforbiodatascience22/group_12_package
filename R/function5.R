53a4ca400b870df6b34d0bc30cbfc1c666c93694
DeepthaSri <s210230@student.dtu.dk>
2022-03-21 10:52:39 +0000

    function 5_test run (doesn't work)

--- R/function5.R
+++ R/function5.R
@@ -1,27 +1,27 @@
 #' Description
 #'
-#' @param n 
+#' @param n
 #'
 #' @return name_me2
 #' @export
 #'
 #' @examples
-name_me1 <- function(name_me2){
-  name_me3 <- name_me2 %>% 
+plot_aa <- function(protein_seq){
+  unique_aa <- protein_seq %>%
     stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) %>%
     as.character() %>%
     unique()
-  
-  counts <- sapply(name_me3, function(name_me4) stringr::str_count(string = name_me2, pattern =  name_me4)) %>% 
+
+  counts <- sapply(unique_aa, function(name_me4) stringr::str_count(string = protein_seq, pattern =  name_me4)) %>%
     as.data.frame()
-  
+
   colnames(counts) <- c("Counts")
-  counts[["Name_me2"]] <- rownames(counts)
-  
-  name_me4 <- counts %>% 
-    ggplot2::ggplot(ggplot2::aes(x = Name_me2, y = Counts, fill = Name_me2)) +
+  counts[["ptr_seq"]] <- rownames(counts)
+
+  name_me4 <- counts %>%
+    ggplot2::ggplot(ggplot2::aes(x = ptr_seq, y = counts, fill = ptr_seq)) +
     ggplot2::geom_col() +
     ggplot2::theme_bw()
-  
+
   return(name_me4)
 }
